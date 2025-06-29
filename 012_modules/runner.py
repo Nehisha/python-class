83a968d88ff0ef0af6d59c4745b89775340f216a
Nehisha Hirachan <nehishahirachan@gmail.com>
2025-06-29 12:36:53 +0545

    Add modules lesson

---
+++ 012_modules/runner.py
@@ -0,0 +1,8 @@
+import calculator
+
+if __name__ == "__main__":
+    print(f"This name of the file is {__name__}")
+    print(calculator.sum(6, 2))
+    print(calculator.subtract(4, 2))
+    print(calculator.multiply(3, 3))
+    print(calculator.dividie(2, 2))
